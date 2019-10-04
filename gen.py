f0c098b4dc863ec393a7e276b25b9a86b7911f9a
schifzt <tagashira.fumito@gmail.com>
2019-10-05 00:50:43 +0900

    committed Sat Oct  5 00:50:43     2019

--- gen.py
+++ gen.py
@@ -9,10 +9,12 @@ s = "## Statistics & Machine Learning"
 for d in dirs:
     # d内のpng画像を取得
     imgs = [f.name for f in os.scandir("./{0}".format(d)) if f.name.endswith(".png")]
+
+    # png画像があればREADMEに記入
     if not imgs==[]:
         s = s+"""
-### [{0}]("./{0}")
-![img]("./{0}/{1}")
+### [{0}]("/{0}")
+![img]("/{0}/{1}")
 """.format(d, imgs[0])
 
 with open("./README.md", mode="w") as f:
